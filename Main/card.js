c23e759fe84a4f29699aba6608f77755d2bdbca2
andersonfant100-hash <andersonfant100@gmail.com>
2025-11-16 23:12:22 -0600

    Update card.js

--- Main/card.js
+++ Main/card.js
@@ -1,12 +1,11 @@
-// Steam-style tilt effect for all cards
 document.querySelectorAll('.card').forEach(card => {
     card.addEventListener('mousemove', e => {
         const rect = card.getBoundingClientRect();
         const x = e.clientX - rect.left;
         const y = e.clientY - rect.top;
 
-        const rotateY = ((x / rect.width) - 0.5) * 15; // horizontal tilt
-        const rotateX = ((y / rect.height) - 0.5) * -15; // vertical tilt
+        const rotateY = ((x / rect.width) - 0.5) * 15;
+        const rotateX = ((y / rect.height) - 0.5) * -15;
 
         card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1.03)`;
         card.style.boxShadow = `0 20px 50px rgba(29,185,84,0.5)`;
